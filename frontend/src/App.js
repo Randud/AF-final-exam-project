c12924d701619f843e29474feab89d831539e4ac
Randud <randu612@gmail.com>
2020-06-13 15:02:52 +0530

    Nav bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './page/Home';
+import {AboutUs} from './page/AboutUs';
+import {NoMatch} from './page/NoMatch';
+import {Layout} from './components/Layout';
+import {Navibar} from './components/Navibar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navibar />
+        <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/" component={AboutUs} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
